0ed0763b3e1ad1f0bc24d9ccf200b4a9f182f25b
paulinasilveira <paulina.silveira@correo.ucu.edu.uy>
2025-10-19 13:24:22 -0300

    Agrego clases Venta y Cotización

---
+++ src/Library/Cotizacion.cs
@@ -0,0 +1,25 @@
+namespace Library;
+
+public class Cotizacion
+{
+    public string Producto { get; set; }
+    public int Precio { get; set; }
+    public DateTime Fecha { get; set; }
+    public string Estado { get; set; }
+    public Cliente Cliente { get; set; }
+
+    public Cotizacion(Cliente cliente, int precio, string producto)
+    {
+        Cliente = cliente;
+        Precio = precio;
+        Producto = producto;
+        Fecha = DateTime.Today;
+        Estado = "Abierta";
+    }
+
+    public void CerrarVenta()
+    {
+        Estado = "Cerrada";
+        
+    }
+}
