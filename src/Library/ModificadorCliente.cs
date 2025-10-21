32138975e91564c432a4ec1f6256272ee17ae4b7
Luutyy <lautaramirezlopez@gmail.com>
2025-10-20 22:44:53 -0300

    Modificar cliente

--- src/Library/ModificadorCliente.cs
+++ src/Library/ModificadorCliente.cs
@@ -2,22 +2,22 @@ namespace Library;
 
 public static class ModificadorCliente
 {
-    static void ModificarNombre(string nombre, Cliente cliente)
+    public static void ModificarNombre(string nombre, Cliente cliente)
     {
         cliente.CambiarNombre(nombre);
     }
     
-    static void ModificarApellido(string apellido, Cliente cliente)
+    public static void ModificarApellido(string apellido, Cliente cliente)
     {
         cliente.CambiarApellido(apellido);
     }
     
-    static void ModificarTelefono(string telefono, Cliente cliente)
+    public static void ModificarTelefono(string telefono, Cliente cliente)
     {
         cliente.CambiarTelefono(telefono);
     }
     
-    static void ModificarEmail(string email, Cliente cliente)
+    public static void ModificarEmail(string email, Cliente cliente)
     {
         cliente.CambiarEmail(email);
     }
